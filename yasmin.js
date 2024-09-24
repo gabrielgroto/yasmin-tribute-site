66e3afc5b03b26d7abafa9c7ca63acb498a47ab8
Dev Gabriel <139563275+DevGab1@users.noreply.github.com>
2024-09-24 13:18:42 -0300

    Update yasmin.js
    Melhoria no js, coração de erro ortográfico

--- yasmin.js
+++ yasmin.js
@@ -3,11 +3,11 @@ const nameM = prompt("Coloque aqui o nome da mulher do Gabriel:")
 bnt.addEventListener("click", functionClick);
 function functionClick (event) {
     event.preventDefault();
-    if (nameM === 'Yasmin Kuhnen') {
-        alert(`O nome da mulher da sua vida está correto, ela é a ${nameM}!`)
+    if (nameM === 'Yasmin Kuhnen' && 'yasmin' && 'yasmin kuhnen') {
+        alert(`O nome da mulher do Gabriel está correto, ela é a ${nameM}!`)
     }
     else {
-        alert('O nome da mulher da sua vida está incorreto! Corrige essa porra.')
+        alert('O nome da mulher do Gabriel está incorreto! Corrige essa porra.')
         return false
     }
 }
