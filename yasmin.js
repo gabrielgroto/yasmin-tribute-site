6297883a5140dbf3d086dbea7f258e05579f91e6
Dev Gabriel <139563275+DevGab1@users.noreply.github.com>
2024-09-24 13:40:01 -0300

    Update yasmin.js

--- yasmin.js
+++ yasmin.js
@@ -1,13 +1,19 @@
 const bnt = document.querySelector("#btnPesquisar");
-const nameM = prompt("Coloque aqui o nome da mulher do Gabriel:")
+const nameM = prompt("Coloque aqui o nome da mulher do Gabriel:");
+const textDiv = document.querySelector(".texto");
+const text = document.createElement("h1");
 bnt.addEventListener("click", functionClick);
 function functionClick (event) {
     event.preventDefault();
     if (nameM === 'Yasmin Kuhnen') {
-        alert(`O nome da mulher do Gabriel está correto, ela é a ${nameM}!`)
+        alert(`O nome da mulher do Gabriel está correto, ela é a ${nameM}!`);
+        textoDiv.appendChild(text);
+        text.innerText('Te amo!');
     }
     else {
-        alert('O nome da mulher do Gabriel está incorreto! Corrige essa porra.')
+        alert.error('O nome da mulher do Gabriel está incorreto! Corrige essa porra.');
+        textoDiv.appendChild(text);
+        text.innerText('Sai daqui cadela');
         return false
     }
 }
