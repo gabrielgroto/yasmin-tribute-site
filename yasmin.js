2ff13d08037f67303b9f9cf3da33ec9100a28f0c
Dev Gabriel <139563275+DevGab1@users.noreply.github.com>
2024-09-24 13:22:45 -0300

    Update yasmin.js

--- yasmin.js
+++ yasmin.js
@@ -3,7 +3,7 @@ const nameM = prompt("Coloque aqui o nome da mulher do Gabriel:")
 bnt.addEventListener("click", functionClick);
 function functionClick (event) {
     event.preventDefault();
-    if (nameM === 'Yasmin Kuhnen' && 'yasmin' && 'yasmin kuhnen') {
+    if (nameM === 'Yasmin Kuhnen') {
         alert(`O nome da mulher do Gabriel está correto, ela é a ${nameM}!`)
     }
     else {
